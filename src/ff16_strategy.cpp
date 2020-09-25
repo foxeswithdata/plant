7d11e26ad2ac735e5af44c51a78a1da49eb6f316
foxeswithdata <elisa@foxeswithdata.com>
2020-09-25 18:00:14 +1000

    Adding fixes to initialise non ES20 strategies

--- src/ff16_strategy.cpp
+++ src/ff16_strategy.cpp
@@ -534,6 +534,7 @@ void FF16_Strategy::prepare_strategy() {
 
 void FF16_Strategy::initialize_states(Internals &vars){
   vars.set_state(state_index.at("height"), height_0);
+  update_dependent_aux(HEIGHT_INDEX, vars);
 }
 
 FF16_Strategy::ptr make_strategy_ptr(FF16_Strategy s) {
